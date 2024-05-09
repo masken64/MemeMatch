15f85e0fefd27f48e53e09c5691050d4de0348a8
masken64 <shrkartik2001@gmail.com>
2024-05-09 17:43:20 +0530

    create game logic and add ui

---
+++ MemeMatch/src/header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return(
+    <div className='header'>
+        <div className='lh'>
+            <div className='title1'>Meme</div><div className="title2">Match</div>
+        </div>
+        <div className='rh'>
+            <a href='https://github.com/masken64/MemeMatch' target='_blank' className='vc'>View Code</a>
+        </div>
+    </div>)
+}
--- MemeMatch/src/main.jsx
+++ MemeMatch/src/main.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import Header from './header'
+import Play from './play'
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <><Header></Header>
+  <Play></Play>
+  </>
 )
